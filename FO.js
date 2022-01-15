0817c54cff8dadcfe433212dea6b4ac604af5aa0
Mohit15985001 <nathwanimohit15@gmail.com>
2022-01-15 23:32:15 +0530

    updates

--- FO.js
+++ FO.js
@@ -1,6 +1,18 @@
-let input = process.argv[2]
+// File System Organizer//
+//Features of the Project -
+//If you have numerous Files in a folder and they are not Properly arranged
+//So you can use this tool to arrange them in specific directory according to their extension
+// like text files will go into text File Folder .exe files will go into application folder and so on
+// so at the end you will have a arranged set of files in specific folders
 
-let command = input
+
+let inputArr = process.argv.slice(2);
+
+let command = inputArr[0];
+
+// console.log(inputArr);
+// console.log(command);
+
 // console.log(input)
 
 switch(command){
@@ -10,11 +22,19 @@ switch(command){
         case "organize" :
             console.log("organize Implemented");
             break;
-        case "Help":
-            console.log("Help Implemented");
+        case "help":
+            // console.log("Help Implemented");
+            helpfn()
             break;
 
         default : 
             console.log("Please Enter a valid command");
             break;
+}
+
+function helpfn(){
+    console.log(`List of all the Commands-
+    1) Tree Command - node FO.js tree <dirname>
+    2) Organize Command- node FO.js organize <dirname>
+    3) Help Command - node FO.js help`)
 }
