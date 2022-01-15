6e51fe49e5903d225583c290e8e7676ecac84c0e
Mohit15985001 <nathwanimohit15@gmail.com>
2022-01-15 22:40:11 +0530

    file update

--- FO.js
+++ FO.js
@@ -1,3 +1,20 @@
-let input = process.argv[1]
+let input = process.argv[2]
 
-console.log(input)
+let command = input
+// console.log(input)
+
+switch(command){
+    case "tree" :
+        console.log("tree Implemented");
+        break;
+        case "organize" :
+            console.log("organize Implemented");
+            break;
+        case "Help":
+            console.log("Help Implemented");
+            break;
+
+        default : 
+            console.log("Please Enter a valid command");
+            break;
+}
