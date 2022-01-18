7e83279a939aa811b3ece0e74c28f8b978fec641
Mohit15985001 <nathwanimohit15@gmail.com>
2022-01-18 22:02:59 +0530

    File Managment Project

--- FO.js
+++ FO.js
@@ -4,7 +4,8 @@
 //So you can use this tool to arrange them in specific directory according to their extension
 // like text files will go into text File Folder .exe files will go into application folder and so on
 // so at the end you will have a arranged set of files in specific folders
-
+const fs = require("fs");
+const path = require("path");
 
 let inputArr = process.argv.slice(2);
 
@@ -20,7 +21,8 @@ switch(command){
         console.log("tree Implemented");
         break;
         case "organize" :
-            console.log("organize Implemented");
+            organizeFn(inputArr[1]);
+            // console.log("organize Implemented");
             break;
         case "help":
             // console.log("Help Implemented");
@@ -37,4 +39,23 @@ function helpfn(){
     1) Tree Command - node FO.js tree <dirname>
     2) Organize Command- node FO.js organize <dirname>
     3) Help Command - node FO.js help`)
+}
+function organizeFn(dirpath){
+    if(dirpath==undefined){
+        console.log("Please enter a valid Directory path");
+        return;
+    }else{
+        let doesExist= fs.existsSync(dirpath);
+        // console.log(doesExist);
+        if(doesExist==true){
+            despath=path.join(dirpath,"organized_files");
+            if(fs.existsSync(despath)==false){
+                fs.mkdirSync(despath);
+            }else{
+                console.log("this folder already exist");
+            }
+        }else{
+            console.log("please enter a valid path");
+        }
+    }
 }
