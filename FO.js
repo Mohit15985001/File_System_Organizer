735382c42571d4b75404cd00777870faee75fb1d
Mohit15985001 <nathwanimohit15@gmail.com>
2022-01-18 23:53:55 +0530

    organizer completed

--- FO.js
+++ FO.js
@@ -4,7 +4,29 @@
 //So you can use this tool to arrange them in specific directory according to their extension
 // like text files will go into text File Folder .exe files will go into application folder and so on
 // so at the end you will have a arranged set of files in specific folders
+let types = {
+    media: ["mp4", "mkv", "mp3"],
+    archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
+    documents: [
+      "docx",
+      "doc",
+      "pdf",
+      "xlsx",
+      "xls",
+      "odt",
+      "ods",
+      "odp",
+      "odg",
+      "odf",
+      "txt",
+      "ps",
+      "tex",
+    ],
+    app: ["exe", "dmg", "pkg", "deb"],
+  };
+  
 const fs = require("fs");
+const { type } = require("os");
 const path = require("path");
 
 let inputArr = process.argv.slice(2);
@@ -58,4 +80,46 @@ function organizeFn(dirpath){
             console.log("please enter a valid path");
         }
     }
+    organizeHelper(dirpath,despath);
+}
+function organizeHelper(src,dest){
+    let childNames= fs.readdirSync(src)
+    for( let i=0;i<childNames.length;i++){
+        let childAddress = path.join(src,childNames[i])
+        let isFile = fs.lstatSync(childAddress).isFile()
+        // console.log(childAddress+" ->" +isFile)
+        if(isFile==true){
+            let filecategory = getCategory(childNames[i]);
+            console.log(childNames[i]+" belongs to "+ filecategory)
+            sendFiles(childAddress,dest,filecategory)
+        }
+    }
+}
+function getCategory(name){
+    let ext = path.extname(name)
+    ext=ext.slice(1)
+    // console.log(ext)
+    for(let type in types){
+        let cTypeArr = types[type];
+        // console.log(cTypeArr);
+        for(let i=0;i<cTypeArr.length;i++){
+            if(ext==cTypeArr[i]){
+                return type;
+            }
+        }
+    }
+    return "others"
+}
+function sendFiles(srcFilePath, dest ,filecategory){
+    let catPath = path.join(dest,filecategory)
+    if(fs.existsSync(catPath)==false){
+        fs.mkdirSync(catPath)
+    }
+    let filename = path.basename(srcFilePath)
+    let destFilePath = path.join(catPath, filename)
+
+    fs.copyFileSync(srcFilePath,destFilePath)
+    fs.unlinkSync(srcFilePath)
+    console.log(filename+" is copied to"+ filecategory)
+
 }
